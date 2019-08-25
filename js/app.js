cf6862b91d057491486bef65d23c56968684c94e
AKINOLA Victor <akinolavictor26@gmail.com>
2019-08-25 22:27:01 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@
  */
 
 // The names and URLs to all of the feeds we'd like available.
-// The are arrayed as objects
+// The are arrayed as objects.
 var allFeeds = [
     {
         name: 'Udacity Blog',
