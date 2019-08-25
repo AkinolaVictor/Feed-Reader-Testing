3be9c4b093e2942440edc0a90a90560bfdbee8d0
AKINOLA Victor <akinolavictor26@gmail.com>
2019-08-25 21:51:41 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -20,6 +20,9 @@ var allFeeds = [
     }, {
         name: 'Linear Digressions',
         url: 'http://feeds.feedburner.com/udacity-linear-digressions'
+    }, {
+        name: 'ML Tutorial',
+        url: 'http://feeds.feedburner.com/udacity-machine-learning'
     }
 ];
 
